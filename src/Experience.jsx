ef4b29c03f9b4b8e028dbff1333da0f063ec1e9c
Su-z-ana <suzanaruth24@gmail.com>
2024-07-09 03:42:42 +0530

    commit

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -42,7 +42,7 @@ export default function Experience()
                         position={ [ 0, 1.56, - 1.4 ] }
                         rotation-x={ - 0.256 }
                     >
-                        <iframe src="about.html"/>
+                        <iframe src="https://su-z-ana.github.io/about/about.html"/>
                     </Html>
                 </primitive>
 
